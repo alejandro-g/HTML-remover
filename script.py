631fa78edb544e9f4c2d0856857ff5da97f7079f
alejandro-g <diego.galdamez@unitec.edu>
2020-03-10 11:06:38 -0600

    agregando regex para remover html

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 #extrae html de una X pagina y luego guarda su html en un input.txt
 import requests
+import re
 
 r = requests.get("https://example.com")
 
@@ -7,3 +8,9 @@ text_file = open("Input.txt", "w")
 text_file.write(r.text)
 text_file.close()
 
+r = "<p class='important'>This is an important text</p>"
+
+def striphtml(r): 
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', r)
+    return cleantext
